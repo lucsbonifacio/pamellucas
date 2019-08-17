cbac94b0fea01fb555ad1a591fdbd02b684db091
Lucas Bonifacio <lucsbonifacio@gmail.com>
2019-08-18 00:15:34 +0200

    🐛 Fix router error

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Home from './pages/home';
 import Thanks from './pages/thank-you';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="pamellucas">
         <Route path="/" exact component={Home} />
         <Route path="/obrigado" component={Thanks} />
+        <Route component={Home} />
       </Router>
     </div>
   );
