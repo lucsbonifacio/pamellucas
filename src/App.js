08c3cfba921417b4fcbe01c338490ef644da09a6
Lucas Bonifacio <lucsbonifacio@gmail.com>
2019-08-18 00:19:25 +0200

    🐛 Add Switch in the router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Home from './pages/home';
 import Thanks from './pages/thank-you';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router basename="pamellucas">
-        <Route path="/" exact component={Home} />
-        <Route path="/obrigado" component={Thanks} />
-        <Route component={Home} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/obrigado" component={Thanks} />
+          <Route component={Home} />
+        </Switch>
       </Router>
     </div>
   );
