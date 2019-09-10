a133762439e415ae5f7ab39db44b63ffb446388f
Lucas Bonifacio <lucsbonifacio@gmail.com>
2019-09-10 21:15:54 +0200

    ✨ Heroku setup

---
+++ build/precache-manifest.1910f52968e0fb6823c38f132103e436.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bb218c79da189d89cd738c5f69284d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdb84d918105ef6b0d71",
+    "url": "/static/css/main.8ceb59ba.chunk.css"
+  },
+  {
+    "revision": "8a795d1b726c74caade8",
+    "url": "/static/js/2.f9df16ae.chunk.js"
+  },
+  {
+    "revision": "bdb84d918105ef6b0d71",
+    "url": "/static/js/main.12a0cd97.chunk.js"
+  },
+  {
+    "revision": "c149603df0129d20fadd",
+    "url": "/static/js/runtime~main.2df2340e.js"
+  },
+  {
+    "revision": "5ee1cb9fe1121733aece8b3726316033",
+    "url": "/static/media/croissant.5ee1cb9f.png"
+  },
+  {
+    "revision": "ae7aa564dccb65299ab8909b07c868b5",
+    "url": "/static/media/jantar.ae7aa564.png"
+  },
+  {
+    "revision": "15f230a9d465c7647283d80e5efb08a3",
+    "url": "/static/media/livros.15f230a9.png"
+  },
+  {
+    "revision": "2ca58e3cd2480fdd2a0e7e70ac4e5a79",
+    "url": "/static/media/otama-ep-webfont.2ca58e3c.woff2"
+  },
+  {
+    "revision": "31108306c5323d9bdde9162526014b84",
+    "url": "/static/media/otama-ep-webfont.31108306.woff"
+  },
+  {
+    "revision": "d6d847c767fb4184d9b165d56a442d93",
+    "url": "/static/media/patinete.d6d847c7.png"
+  },
+  {
+    "revision": "926f4ec1529cb1eac54169aa8e2d4949",
+    "url": "/static/media/piquenique.926f4ec1.png"
+  },
+  {
+    "revision": "510b83fd99a4cb9b50b13bdf719fdca5",
+    "url": "/static/media/supermercado.510b83fd.png"
+  }
+]);
